f957ceceaccfc864889c4fea863c9d2f083a9b98
UdodOleg <udodolegofznu@gmail.com>
2023-06-19 14:20:44 +0300

    Added session migration table for dynamic lists

--- Web_Programming_Py_JS/python/lecture3/tasks/views.py
+++ Web_Programming_Py_JS/python/lecture3/tasks/views.py
@@ -1,20 +1,25 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-tasks = ["foo","bar","baz"]
+from django.urls import reverse
+
 # Create your views here.
 class NewTaskForm(forms.Form):
     task=forms.CharField(label="New Task")
     # priority = forms.IntegerField(label="Priority", min_value=0, max_value=100)
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
     return render(request,"tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request,"tasks/add.html", {
                 "form": form
