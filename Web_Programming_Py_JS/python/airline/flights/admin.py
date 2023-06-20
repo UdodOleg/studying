38936053ecdf969acd34aafcac9a591a1e3a9116
UdodOleg <udodolegofznu@gmail.com>
2023-06-20 22:44:11 +0300

    Added dynamic flight list and passenger DJANGO

--- Web_Programming_Py_JS/python/airline/flights/admin.py
+++ Web_Programming_Py_JS/python/airline/flights/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Flight,AirPort,Passenger
 # Register your models here.
+admin.site.register(AirPort)
+admin.site.register(Flight)
+admin.site.register(Passenger)
--- Web_Programming_Py_JS/python/airline/flights/models.py
+++ Web_Programming_Py_JS/python/airline/flights/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class AirPort(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+    
+    def __str__(self) -> str:
+        return f"{self.city} ({self.code})"
+class Flight(models.Model):
+    origin = models.ForeignKey(AirPort,on_delete=models.CASCADE,related_name="departures")
+    destination = models.ForeignKey(AirPort,on_delete=models.CASCADE,related_name="arrivals")
+    duration = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return f"{self.id} : from {self.origin} to {self.destination}!"
+class Passenger(models.Model):
+    first = models.CharField(max_length=64, default="")
+    last = models.CharField(max_length=64, default="")
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers", default="")
+    
+    def __str__(self):
+        return f"{self.first} {self.last}"
